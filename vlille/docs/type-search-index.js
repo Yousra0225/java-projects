9cfa033fcad61a925af1acaf3a55f0c223038897
Yousra0225 <chbibyousra02@gmail.com>
2025-03-04 15:19:08 +0100

    feat : generate documentation javadoc

---
+++ vlille/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"vlille","l":"Bike"},{"p":"vlille","l":"BikeMain"},{"p":"vlille.util","l":"BikeModel"},{"p":"vlille","l":"BikeNotAvailableException"},{"p":"vlille","l":"BikeStation"},{"p":"vlille","l":"BikeStationMain"}];updateSearchResults();

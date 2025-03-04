8afdca04721525dc225f68696555883f4c60af56
Yousra0225 <chbibyousra02@gmail.com>
2025-03-04 10:40:16 +0100

    feat: add argument verification and bike retrieval logic

--- vlille/src/vlille/BikeStationMain.java
+++ vlille/src/vlille/BikeStationMain.java
@@ -15,8 +15,24 @@ public class BikeStationMain{
         System.out.println("bike 1 of id: "+bike1.getId()+", of model : "+bike1.getModel());
         System.out.println("bike 2 of id: "+bike1.getId()+", of model : "+bike2.getModel());
         
-       
-            }         
+        //  Verification if the parameter is passed as argument 
+        if (args.length>0){
+            // Recuperation of the value of argument, and conversion to a int
+            int n = Integer.parseInt(args[0]);
+
+            // try to get the bike at the position n
+            try{
+                Bike getBike= station.takeBike(n);
+                System.out.println("The bike with ID : " + getBike.getId());
+            }
+            catch (BikeNotAvailableException e) {
+                System.out.println("The bike is not available at the position : "+n);
+            }
+        }
+        else{
+                System.out.println("No argument provided, please set an argument");
+            }    
+
         
 
 }}

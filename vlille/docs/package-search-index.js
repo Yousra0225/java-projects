2bdfe176e69f91427604abe36ed9458324ba6a3e
Yousra0225 <chbibyousra02@gmail.com>
2025-03-16 00:01:33 +0100

    fix: remove docs file

--- vlille/docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"vlille"},{"l":"vlille.util"}];updateSearchResults();
--- vlille/docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"vlille","l":"Bike"},{"p":"vlille","l":"BikeMain"},{"p":"vlille.util","l":"BikeModel"},{"p":"vlille","l":"BikeNotAvailableException"},{"p":"vlille","l":"BikeStation"},{"p":"vlille","l":"BikeStationMain"}];updateSearchResults();
